5756ea32011b1e411a6a1d1d8edb724482f8fdbb
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-05-21 17:58:32 -0400

    Manage exceptions

--- app/services/random_users_requester.rb
+++ app/services/random_users_requester.rb
@@ -1,14 +1,22 @@
 class RandomUsersRequester < ApplicationService
   def call
     url_api = ENV['RANDOMUSER_API_URL']
-    response = HTTP.get(url_api)
-    if response.code == 200
-      random_users = JSON.parse(response)
-      results = random_users['results'].map{|person| person }
-      Rails.cache.write(:results, results, expires_in: 1.minutes)
+    http_config = {:read => 5, :write => 5, :connect => 5}
+    begin
+      response = HTTP.get(url_api).timeout(http_config)
+    rescue
+      Rails.logger.error("External API(#{url_api}) not responds...")
     else
-      results = Rails.cache.read(:results)
-      Rails.cache.write(:results, results, expires_in: 1.minutes)
+      if response.code == 200
+        random_users = JSON.parse(response)
+        results = random_users['results']
+        Rails.cache.write(:results, results, expires_in: 1.minutes)
+      else
+        Rails.logger.warning("Response code #{response.code} from API...")
+        Rails.logger.warning("Not data for cache renewal, keeping the last ones...")
+        results = Rails.cache.read(:results)
+        Rails.cache.write(:results, results, expires_in: 1.minutes)
+      end
     end
   end
 end
