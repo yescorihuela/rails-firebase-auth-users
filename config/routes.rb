591dff7207372130479e477855e9fc6897a33393
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-05-21 12:49:58 -0400

    Add JWT validation via concern

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       get '/random/users/show/', to: 'random_users_data#show', as: :random_users_data_show
-      get '/users/check_user/', to: 'users#check_user', as: :users_check_user
     end
   end
 end
